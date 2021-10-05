beb19539b0ab68d73bbae49a2caddd85492a4293
Dustin Simensen <dustinsimensen@gmail.com>
2021-10-04 17:47:37 -0700

    Turned off pages and only have under construction page. Other pages can still be navigated to, but it shouldn't be a big deal.

--- components/Layout.js
+++ components/Layout.js
@@ -9,11 +9,11 @@ export default function Layout({ children, data, category = null }) {
         <title>Brooke Metropulos</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar data={data?.navbar} category={category} />
+      {/* <Navbar data={data?.navbar} category={category} /> */}
       <main className="flex-1 overflow-y-auto pt-16 pb-4 px-4  pt-4 mb-auto md:px-12 md:pt-24 3xl:container 3xl:mx-auto 3xl:px-20 3xl:pt-28">
         {children}
       </main>
-      <Footer data={data?.footer} />
+      {/* <Footer data={data?.footer} /> */}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,29 @@ import Link from 'next/link';
 import Layout from '../components/Layout';
 import { baseUrl, fetchQuery } from '../lib/fetchQuery';
 import findUrl from '../lib/findUrl';
+import CustomImage from '../components/CustomImage';
 
 export default function Home({ globalData, pageData }) {
   const { buttons } = pageData;
+  console.log(pageData.image.url);
   return (
     <Layout data={globalData}>
-      <section className="flex flex-wrap items-start justify-evenly h-full">
-        {buttons &&
+      {/* <section className="flex flex-wrap items-start justify-evenly h-full"> */}
+      {/* This following section is the temp section tag/class until our site is turned on. */}
+      <section className="flex flex-col items-center justify-center h-full text-lg font-gopher font-bold  md:text-4xl  md:mr-auto xl:text-4xl 2xl:text-5xl 3xl:text-6xl text-red">
+        <h1 className="">THIS SITE IS UNDER CONSTRUCTION!</h1>
+        <div className="mx-auto w-full md:w-7/12 xl:w-2/5 xl:-ml-0 xl:-mr-0">
+          <CustomImage
+            url={pageData.image.url}
+            width={698}
+            height={666}
+            bgHeight={91}
+            effect={'blur:500'}
+            sizes="(max-width: 600px) 90vw, (max-width: 1200px) 33vw, 33vw"
+          />
+        </div>
+
+        {/* {buttons &&
           buttons.map((button, index) => {
             const url = findUrl(button.svg.url);
             const textColor = `text-${button.textColor}`;
@@ -29,7 +45,7 @@ export default function Home({ globalData, pageData }) {
                 </a>
               </Link>
             );
-          })}
+          })} */}
       </section>
     </Layout>
   );
@@ -37,7 +53,7 @@ export default function Home({ globalData, pageData }) {
 
 export async function getStaticProps() {
   const globalData = await fetchQuery('global');
-  const pageData = await fetchQuery('homepage');
+  const pageData = await fetchQuery('contact-page');
   return {
     props: {
       globalData,
